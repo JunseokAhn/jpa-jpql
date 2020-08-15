ce16898f1688cc2886188bd9cd415f48053374ef
JunseokAhn <57289429+JunseokAhn@users.noreply.github.com>
2020-08-15 11:12:33 +0900

    Add jpql

---
+++ src/main/java/Jpql/JpaMain.java
@@ -0,0 +1,30 @@
+package Jpql;
+
+import Jpql.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory EMF = Persistence.createEntityManagerFactory("hello");
+        EntityManager EM = EMF.createEntityManager();
+        EntityTransaction ET = EM.getTransaction();
+
+        ET.begin();
+        List<Member> member = EM.createQuery(
+                "select m from Member m where m.name like '%kim%'",
+                Member.class).getResultList();
+        for (Member i : member) {
+            System.out.println(i);
+        }
+        ET.commit();
+        EM.close();
+        EMF.close();
+    }
+}
